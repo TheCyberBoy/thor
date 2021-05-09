2d61c9054ffd880aef23ba56f63d63bed4b41fd4
Derek <65240836+TheCyberBoy@users.noreply.github.com>
2021-05-09 13:22:00 +0530

    Update thor.py

--- thor.py
+++ thor.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------------------------
-# Thor - HTTP Unbearable Load Derek
+# Thor - The operation red
 #
 # this tool is a dos tool that is meant to put heavy load on HTTP servers in order to bring them
 # to their knees by exhausting the resource pool, its is meant for research purposes only
